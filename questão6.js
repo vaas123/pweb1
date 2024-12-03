571fb63472b41d18509967cb65f2fc165c252e3d
vaas123 <vaas1@aluno.ifal.edu.br>
2024-12-02 22:52:35 -0300

    Create questão6.js

---
+++ questão6.js
@@ -0,0 +1,7 @@
+let tempF = parseFloat(prompt("Digite a sua temperatura em Fahrenheit"));
+
+let conversao = (tempF - 32) * 5 / 9;
+
+let tempCelsius = conversao;
+
+alert (" A temperaura de Fahrenheit: " + tempF + "F" + ' para celsius é de: ' + tempCelsius + '°C');
