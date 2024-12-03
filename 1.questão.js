a1ff36d3bf2a8958c27cd440914a7b1a3ac77162
vaas123 <vaas1@aluno.ifal.edu.br>
2024-12-02 22:34:29 -0300

    Create 1.questão.js

---
+++ 1.questão.js
@@ -0,0 +1,8 @@
+function media(){
+    let n1 = parseFloat(document.getElementById("n1").value);
+    let n2 = parseFloat(document.getElementById("n2").value);
+    let n3 = parseFloat(document.getElementById("n3").value);
+    let media = (n1+n2+n3)/3;
+
+    alert(media);
+}
