1385f85e1f587c960fc5925e07a67b29f89da301
vaas123 <vaas1@aluno.ifal.edu.br>
2024-12-02 22:41:27 -0300

    Create questão12.js

---
+++ questão12.js
@@ -0,0 +1,13 @@
+let nota1 = parseFloat(prompt("Digite a nota 1:"));
+let nota2 = parseFloat(prompt("Digite a nota 2:"));
+let nota3 = parseFloat(prompt("Digite a nota 3:"));
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+  alert("ALUNO APROVADO");
+
+} else if(media =5  <7){
+  alert("ALUNO EM RECUPERACÃO");
+} else {
+    alert("ALUNO RPROVADO")
+}
