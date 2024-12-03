4b72da45216e05aa18420fc8cf77f7f83ea62af1
vaas123 <vaas1@aluno.ifal.edu.br>
2024-12-02 22:50:14 -0300

    Create questão5.js

---
+++ questão5.js
@@ -0,0 +1,9 @@
+let ValorProduto = parseFloat(prompt("Digite um valor para o Produto: "));
+
+let reajuste = ValorProduto * 0.3333;
+
+let somaDeValores = ValorProduto + reajuste;
+
+alert("O Valor do reajuste é : " + reajuste);
+
+alert("O valor final com o reajuste é : " + somaDeValores);
